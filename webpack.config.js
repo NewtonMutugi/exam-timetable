c60ec04a94a8e4871cdd5a3bb19c3109a19109d0
NewtonMutugi <newtonmut197@gmail.com>
2024-04-03 13:38:02 +0300

    Configured webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+  },
+  optimization: {
+    minimize: false,
   },
 };
